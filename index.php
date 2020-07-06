3f1e45fb12d479c1b700925224d354f87f68b82e
Albertaugustine <yesuarielalbert@gmail.com>
2020-07-06 23:22:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html >
-<html  ng-app = 'listData'>
+<!DOCTYPE html ng-app = 'listData'>
   <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
